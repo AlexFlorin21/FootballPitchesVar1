0fc7e3880d9562bad92bc41ceefc9223b039977d
AlexFlorin21 <119683630+AlexFlorin21@users.noreply.github.com>
2023-11-20 20:00:25 +0200

    Add files via upload

---
+++ ProjectVar3/BookingController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Football.Data;
+using Football.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Football.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingController(ApplicationDbContext context)
+        {
+            // Constructorul injectează contextul bazei de date pentru a fi utilizat în controller.
+            _context = context;
+        }
+
+        // GET: api/Booking
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Booking>>> GetAllBookings()
+        {
+            // Returnează toate rezervările din baza de date.
+            return await _context.Bookings.ToListAsync();
+        }
+
+        // GET: api/Booking/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Booking>> GetBooking(int id)
+        {
+            // Caută o rezervare specifică în baza de date folosind ID-ul.
+            var booking = await _context.Bookings.FindAsync(id);
+
+            // Dacă rezervarea nu este găsită, returnează un răspuns NotFound (404).
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            // Returnează rezervarea găsită.
+            return booking;
+        }
+
+        // POST: api/Booking
+        [HttpPost]
+        public async Task<ActionResult<Booking>> CreateBooking(Booking booking)
+        {
+            // Adaugă o nouă rezervare în baza de date.
+            _context.Bookings.Add(booking);
+            await _context.SaveChangesAsync();
+
+            // Returnează detaliile rezervării create împreună cu un cod de status 201 (Created).
+            return CreatedAtAction(nameof(GetBooking), new { id = booking.Id }, booking);
+        }
+
+        // PUT: api/Booking/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBooking(int id, Booking booking)
+        {
+            // Verifică dacă ID-ul din URL corespunde cu cel al rezervării.
+            if (id != booking.Id)
+            {
+                return BadRequest();
+            }
+
+            // Marchează entitatea ca fiind modificată.
+            _context.Entry(booking).State = EntityState.Modified;
+
+            try
+            {
+                // Salvează modificările în baza de date.
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                // Verifică dacă rezervarea există în cazul unei excepții de concurență.
+                if (!BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            // Returnează un răspuns NoContent (204) după actualizare.
+            return NoContent();
+        }
+
+        // DELETE: api/Booking/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBooking(int id)
+        {
+            // Caută rezervarea care trebuie ștearsă.
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            // Șterge rezervarea din baza de date.
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+
+            // Returnează un răspuns NoContent (204) după ștergere.
+            return NoContent();
+        }
+
+        private bool BookingExists(int id)
+        {
+            // Verifică dacă există o rezervare cu ID-ul specificat în baza de date.
+            return _context.Bookings.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ProjectVar3/User.cs
@@ -0,0 +1,115 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
+namespace Football.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Numele de utilizator este obligatoriu.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Adresa de email este obligatorie.")]
+        [EmailAddress(ErrorMessage = "Adresa de email nu este validă.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Parola este obligatorie.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Parolele nu se potrivesc.")]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+    }
+    public class User :IdentityUser
+    {
+
+        public string? FullName { get; set; }
+
+        // Timestamp-uri pentru audit
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int UpdatedByUserId { get; set; } // ID-ul utilizatorului care a efectuat ultima actualizare
+
+        public string? ResetToken { get; set; } // Token pentru resetarea parolei
+        public DateTime? ResetTokenExpiration { get; set; } // Data de expirare a tokenului pentru resetarea parolei
+
+        // Relația One-to-Many cu Booking
+        public ICollection<Booking> Bookings { get; set; }
+
+        // Relația Many-to-Many cu Field
+        public ICollection<UserField> UserFields { get; set; }
+
+        // Proprietate pentru relația One-to-One cu Coupon
+        public Coupon? Coupon { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+
+        // Constructor
+        public User()
+        {
+            UserFields = new HashSet<UserField>();
+            Bookings = new HashSet<Booking>();
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+            Reviews = new HashSet<Review>();
+        }
+
+
+
+        // Exemplu de metodă pentru resetarea parolei sau alte operațiuni specifice utilizatorului
+        public void InitiatePasswordReset()
+        {
+            // Generarea unui token pentru resetarea parolei
+            ResetToken = GenerateResetToken();
+
+            // Setarea datei de expirare a tokenului (de exemplu, la 1 oră de la inițiere)
+            ResetTokenExpiration = DateTime.Now.AddHours(1);
+        }
+
+        // Metodă pentru resetarea parolei
+        public void ResetPassword(string newPassword)
+        {
+            if (CanResetPassword())
+            {
+                // Resetarea efectivă a parolei (de exemplu, hashingul noii parole și salvarea ei)
+                PasswordHash = HashPassword(newPassword);
+
+                // Resetarea tokenului și datei de expirare
+                ResetToken = null;
+                ResetTokenExpiration = null;
+            }
+        }
+
+        // Metoda pentru verificarea dacă poate fi resetată parola
+        public bool CanResetPassword()
+        {
+            return ResetTokenExpiration.HasValue && ResetTokenExpiration > DateTime.Now;
+        }
+
+        // Metodă pentru generarea unui token pentru resetarea parolei (simplificată)
+        private string GenerateResetToken()
+        {
+            return Convert.ToBase64String(Guid.NewGuid().ToByteArray()) +
+                   Convert.ToBase64String(RandomNumberGenerator.GetBytes(16));
+        }
+
+        // Metodă pentru hashingul parolei (simplificată)
+        private string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+        public bool VerifyPassword(string inputPassword, string storedHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(inputPassword, storedHash);
+        }
+    }
+}

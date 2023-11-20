ad6884078f9b0857fc6edafbbc90f7865d677343
AlexFlorin21 <119683630+AlexFlorin21@users.noreply.github.com>
2023-11-20 20:08:23 +0200

    Add files via upload

---
+++ ProjectVar3/Migrations/20231119210136_UpdateUserCouponToOneToOne.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Football.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserCouponToOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Coupons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Coupons_UserId",
+                table: "Coupons",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Coupons_Users_UserId",
+                table: "Coupons",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Coupons_Users_UserId",
+                table: "Coupons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Coupons_UserId",
+                table: "Coupons");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Coupons");
+        }
+    }
+}

3c9ace3975ff0bcbfa36c8bae152d927a6cf89e4
AlexFlorin21 <119683630+AlexFlorin21@users.noreply.github.com>
2023-12-18 01:29:23 +0200

    Add files via upload

---
+++ ProjectVar4/20231130135847_SeedInitialData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Football.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Seeding pentru AspNetRoles
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[,]
+                {
+            { Guid.NewGuid().ToString(), "Admin", "ADMIN", Guid.NewGuid().ToString() },
+            { Guid.NewGuid().ToString(), "User", "USER", Guid.NewGuid().ToString() }
+                });
+
+            // Seeding pentru AspNetUsers
+            var adminUserId = Guid.NewGuid().ToString();
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "UserName", "NormalizedUserName", "Email", "NormalizedEmail", "EmailConfirmed", "PasswordHash", "SecurityStamp", "ConcurrencyStamp", "PhoneNumber", "PhoneNumberConfirmed", "TwoFactorEnabled", "LockoutEnabled", "AccessFailedCount", "FullName", "CreatedAt", "UpdatedAt" },
+                values: new object[,]
+                {
+            { adminUserId, "admin@example.com", "ADMIN@EXAMPLE.COM", "admin@example.com", "ADMIN@EXAMPLE.COM", true, "AQAAAAEAACcQAAAAE...hashedpassword...", Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), "1234567890", true, false, false, 0, "Admin User", DateTime.UtcNow, DateTime.UtcNow }
+                });
+
+            // După ce ai inserat utilizatorii, poți continua cu tabelele 'Fields', 'Bookings', etc.
+            // Aici ar trebui să continui cu restul seeding-ului respectând dependențele dintre tabele.
+            migrationBuilder.InsertData(
+        table: "Fields",
+        columns: new[] { "Id", "Name", "Location", "IsIndoor", "Capacity", "SurfaceType", "HasLighting", "AdditionalFacilities" },
+        values: new object[,]
+        {
+            // Adaugă date pentru fiecare teren; asigură-te că fiecare rând are un ID unic
+            { 1, "Teren 1", "Locație 1", false, 100, "Iarbă", true, "Vestiare, Dușuri" },
+            { 2, "Teren 2", "Locație 2", true, 50, "Synthetic", false, "Vestiare" }
+            // Adaugă alte terenuri după necesități
+        });
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ProjectVar4/AccountController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Football.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+
+    public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+        if (model == null || string.IsNullOrEmpty(model.Password))
+        {
+            // Tratarea cazului în care modelul sau parola sunt nule sau goale
+            return BadRequest("Invalid request.");
+        }
+
+        var user = new User { UserName = model.Username, Email = model.Email };
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            // Logica adițională după înregistrarea cu succes
+            return Ok(new { message = "User registered successfully!" });
+        }
+
+        return BadRequest(result.Errors);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        if (model == null || string.IsNullOrEmpty(model.Username) || string.IsNullOrEmpty(model.Password))
+        {
+            // Tratarea cazului în care modelul, numele de utilizator sau parola sunt nule sau goale
+            return BadRequest("Invalid request.");
+        }
+
+
+        var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, isPersistent: false, lockoutOnFailure: false);
+
+        if (result.Succeeded)
+        {
+            // Logica adițională după autentificarea cu succes
+            return Ok(new { message = "User logged in successfully!" });
+        }
+
+        return Unauthorized();
+    }
+}
+[Authorize(Roles = "Admin")]
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController : ControllerBase
+{
+    [HttpGet("admin-action")]
+    public IActionResult AdminAction()
+    {
+        return Ok(new { message = "This is an admin action." });
+    }
+}
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly UserManager<User> _userManager;
+
+    public UsersController(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    // GET: api/Users
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        var users = _userManager.Users.ToList();
+        return Ok(users);
+    }
+
+    // GET: api/Users/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+
+    // POST: api/Users
+    [HttpPost]
+    public async Task<ActionResult<User>> PostUser([FromBody] CreateUserModel model)
+    {
+        if (model == null || string.IsNullOrWhiteSpace(model.Password))
+        {
+            return BadRequest("User or password cannot be null.");
+        }
+
+        var user = new User
+        {
+            UserName = model.Username,
+            Email = model.Email,
+            // Setează aici și alte proprietăți necesare ale user-ului, dacă este cazul
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            // Nu returna obiectul `User` complet, deoarece poate conține informații sensibile
+            // În schimb, returnează un mesaj sau un DTO (Data Transfer Object) simplificat
+            return Ok(new { message = "User created successfully", userId = user.Id });
+        }
+
+        return BadRequest(result.Errors);
+    }
+
+    // DELETE: api/Users/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _userManager.DeleteAsync(user);
+
+        if (result.Succeeded)
+        {
+            return NoContent();
+        }
+
+        return BadRequest(result.Errors);
+    }
+}
+public class CreateUserModel
+{
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    // Adaugă aici alte câmpuri necesare pentru înregistrarea utilizatorului
+}
